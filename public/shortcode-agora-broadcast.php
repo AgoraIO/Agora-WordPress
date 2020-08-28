6aeb0802b17110d6144ce0534c379550f09fbc15
Julian Lamprea <jalamprea@gmail.com>
2020-08-28 14:14:28 -0500

    Validated if  is array or is single value on shortcode

--- public/shortcode-agora-broadcast.php
+++ public/shortcode-agora-broadcast.php
@@ -22,7 +22,8 @@ function renderBroadcastShortcode($agora, $attrs) {
     $current_user = wp_get_current_user();
 
     ob_start();
-    if ( in_array($current_user->ID, $props['host']) ) {
+    $host = is_array($props['host']) ? $props['host'] : array($props['host']);
+    if ( in_array($current_user->ID, $host) ) {
       $agoraUserScript = 'js/agora-broadcast-client.js';
       require_once('views/wp-agora-io-broadcast.php');
     } else {
