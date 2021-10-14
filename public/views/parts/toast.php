bf759a57b889775e95fbeb34518583c596783f12
Kaushal-designer <kaushal.singh@bigsteptech.com>
2021-10-14 13:45:54 +0530

    Resolved Remote streams not showing on full screen in full screen

--- public/views/parts/toast.php
+++ public/views/parts/toast.php
@@ -1,4 +1,4 @@
-<div class="toast" data-delay="5000">
+<div class="toast" data-delay="5000" style="display: none;">
   <div class="toast-header">
     <strong class="mr-auto" id="agora-toast-title"> </strong>
     <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
